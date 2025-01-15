809f1e882e4078203f025696615ac2981493f06d
Springcomp <springcomp@users.noreply.github.com>
2025-01-15 20:41:06 +0100

    [ternary] supports parsing ternary operator expressions

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -352,6 +352,29 @@ class TestMergedLists(unittest.TestCase):
              ["five", "six"], ["seven", "eight"],
              ["nine"], ["ten"]])
 
+class TestTernaryOperatorExpressions(unittest.TestCase):
+    def setUp(self):
+        self.parser = parser.Parser()
+        self.data = {
+            'true': True,
+            'false': False,
+            'foo': 'foo',
+            'bar': 'bar',
+            'baz': 'baz',
+            'qux': 'qux',
+            'quux': 'quux'
+        }
+
+    def test_ternaryOperatorExpression(self):
+        parsed = self.parser.parse('true ? foo : bar')
+        match = parsed.search(self.data)
+        self.assertEqual(match, 'foo')
+
+    def test_nestedTernaryOperatorExpressions(self):
+        parsed = self.parser.parse('foo ? bar ? baz : qux : quux')
+        match = parsed.search(self.data)
+        self.assertEqual(match, 'baz')
+
 
 class TestParserCaching(unittest.TestCase):
     def test_compile_lots_of_expressions(self):
